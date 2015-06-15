40eb33c18e68d013c994661addb68ea9fd854ebc
jayandra <jayandra@umn.edu>
2015-06-15 10:55:17 -0500

    changed database to use mysql
    CREATE USER 'friendfinder'@'localhost' IDENTIFIED BY 'friendfinder';
    
    GRANT ALL PRIVILEGES ON *.* TO 'friendfinder'@'localhost' WITH GRANT OPTION;

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'mysql2'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -45,6 +45,9 @@ end
 
 
 
+group :production do
+  gem 'pg'
+end
 gem 'bootstrap-sass', '~> 3.3.4'
 gem 'devise'
 gem 'simple_form'
--- db/schema.rb
+++ db/schema.rb
@@ -14,29 +14,29 @@
 ActiveRecord::Schema.define(version: 20150606200337) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "sex"
-    t.string   "school"
-    t.string   "highschool"
-    t.string   "city"
-    t.string   "photo"
-    t.integer  "views"
-    t.integer  "likes"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "name",                   limit: 255
+    t.string   "sex",                    limit: 255
+    t.string   "school",                 limit: 255
+    t.string   "highschool",             limit: 255
+    t.string   "city",                   limit: 255
+    t.string   "photo",                  limit: 255
+    t.integer  "views",                  limit: 4
+    t.integer  "likes",                  limit: 4
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.integer  "failed_attempts",        limit: 4,   default: 0,  null: false
+    t.string   "unlock_token",           limit: 255
     t.datetime "locked_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
 end
